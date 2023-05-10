e19beca0b08946d4504d8a46f4f39c1fca6ff7bb
刘云飞 <954326053@qq.com>
2023-05-10 21:29:02 +0800

    '屏蔽暂时无用的模型'些

--- app/store/config.ts
+++ app/store/config.ts
@@ -76,7 +76,7 @@ export const ALL_MODELS = [
     name: "gpt-3.5-turbo-0301",
     available: true,
   },
-  {
+  /*{
     name: "qwen-v1", // 通义千问
     available: false,
   },
@@ -95,7 +95,7 @@ export const ALL_MODELS = [
   {
     name: "chatglm", // chatglm-6b
     available: false,
-  },
+  },*/
 ] as const;
 
 export type ModelType = (typeof ALL_MODELS)[number]["name"];
